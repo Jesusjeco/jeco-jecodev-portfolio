99efffada815a002c4ae94a543570a5af70e57f2
Jesus Carrero <jesusenrique.carrero@gmail.com>
2024-10-12 23:40:05 -0400

    first commit

---
+++ wp-content/themes/jecoWordpressShell/footer.php
@@ -0,0 +1,10 @@
+<?php
+$footer_data = get_field('footer_data', 'option');
+?>
+<footer class="main-footer">
+    <p>footer</p>
+</footer>
+<?php wp_footer() ?>
+</body>
+
+</html>
---
+++ wp-content/themes/jecoWordpressShell/front-page.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main class="front-page" id="main" role="main">
+    <h2>front-page.php</h2>
+    <?php the_content(); ?>
+</main>
+
+<?php get_footer(); ?>
